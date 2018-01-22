c477ff727a5bde75faee3886f308bda91a6aad45
Unknown <wybatt@live.com>
2018-01-22 09:34:05 -0700

    Created Text adventure project

---
+++ Text adventure/Assets/Scripts/script.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Adventure : MonoBehaviour {
+    public enum States { cell, windowm, gate };
+    public States mystate;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
