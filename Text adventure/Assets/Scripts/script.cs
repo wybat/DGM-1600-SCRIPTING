1753b1c75c9d4948843cf1ad96403ea141989757
Unknown <wybatt@live.com>
2018-01-24 09:53:24 -0700

    Text Adventure Begin

--- Text adventure/Assets/Scripts/script.cs
+++ Text adventure/Assets/Scripts/script.cs
@@ -2,17 +2,57 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Adventure : MonoBehaviour {
-    public enum States { cell, windowm, gate };
-    public States mystate;
+public class Script : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public enum States { cell, window, gate, bed };
+    public States currentState;
+
+    // Use this for initialization
+    void Start()
+    {
+        currentState = States.cell;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (currentState == States.cell){Cell();}
+        if (currentState == States.bed){Bed();}
+        if (currentState == States.gate){Gate();}
+    }
+
+    private void Cell()
+    {
+        print("You are in a cell. It's gross and dark and stinky.\n" +
+            "There is a window.\n" +
+            "There is a Gate\n" +
+            "There is a Bed\n" +
+            "Press W for Window, G for Gate, B for Bed.\n");
+        if (Input.GetKeyDown(KeyCode.W)) { currentState = States.window; }
+        if (Input.GetKeyDown(KeyCode.G)) { currentState = States.gate; }
+        if (Input.GetKeyDown(KeyCode.B)) { currentState = States.bed; }
+    }
+    private void Bed()
+    {
+        print("Your Looking at the dark gross bed./n" +
+            "There is a window./n" +
+            "There is a Gate./n" +
+            "There is a Bed./n" +
+            "Press W for Window, G for Gate, B for Bed./n");
+        if (Input.GetKeyDown(KeyCode.W)) { currentState = States.window; }
+        if (Input.GetKeyDown(KeyCode.G)) { currentState = States.gate; }
+        if (Input.GetKeyDown(KeyCode.B)) { currentState = States.bed; }
+    }
+    private void Gate()
+    {
+        print("The Gate is barred and locked... no way your getting through this way./n" +
+            "There is a window./n" +
+            "There is a Gate./n" +
+            "There is a Bed./n" +
+            "Press W for Window, G for Gate, B for Bed./n");
+        if (Input.GetKeyDown(KeyCode.W)) { currentState = States.window; }
+        if (Input.GetKeyDown(KeyCode.G)) { currentState = States.gate; }
+        if (Input.GetKeyDown(KeyCode.B)) { currentState = States.bed; }
+    }
 }
