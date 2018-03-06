e937c2ee3e38ce0aab636d52186aa4f57427c9b6
Unknown <wybatt@live.com>
2018-03-05 17:23:44 -0700

    Brick Breaker work_2

---
+++ Brick Breaker/Assets/_Scripts/Paddle.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour {
+
+    public float velocity;
+    public float boundX;
+    // Use this for initialization
+    void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Transform paddle = GetComponent<Transform>();
+        float mouseX = Input.GetAxis("Mouse X");
+        paddle.position = paddle.position + (Vector3.right * mouseX * velocity * Time.deltaTime);
+        float paddleX = paddle.position.x;
+        paddleX = Mathf.Clamp(paddleX, -boundX, boundX);
+        paddle.position = new Vector3(paddleX, paddle.position.y, paddle.position.z);
+    }
+}
